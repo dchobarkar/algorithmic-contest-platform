e3ae66f0044e01349fee0c3a34b6204ec9d5d705
Darshan Chobarkar <dchobarkar@gmail.com>
2024-08-05 12:54:08 +0530

    update navbar

--- apps/web/components/Appbar.tsx
+++ apps/web/components/Appbar.tsx
@@ -16,7 +16,7 @@ export function Appbar() {
     <header className="bg-gray-900 text-white px-4 md:px-6 py-3 flex items-center justify-between">
       <Link href="/" className="flex items-center gap-2" prefetch={false}>
         <CodeIcon className="h-6 w-6" />
-        <span className="text-lg font-bold">Code100x</span>
+        <span className="text-lg font-bold">DarshanWebDev</span>
       </Link>
 
       <nav className="hidden md:flex items-center gap-6">
